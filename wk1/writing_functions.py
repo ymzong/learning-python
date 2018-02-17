24584a28a76cf15e5fa7a75fbe99ee0063ff65e5
Yifan Cao <9102602+sunnyifan@users.noreply.github.com>
2018-02-16 23:49:41 -0500

    writing_functions: cont'd

--- wk1/writing_functions.py
+++ wk1/writing_functions.py
@@ -29,4 +29,66 @@ def isPositive(x):
 print(isPositive(5))        # Prints Hello! then True
 
 # No return statement--> return None
-# 
+
+# 3. Print vs. Return
+# Common mistake at the beginning
+def cubed(x):
+    print(x ** 3)
+#   return None   # When you don't have return statement, python adds it for you
+cubed(2)                   # Sort of worked
+print(cubed(3))            # Worked but prints None
+# print(2 * cubed(4))      # Error!
+
+def cubed(x):
+    return (x ** 3)
+    
+cubed(2)                   # Computed but not printed 
+print(cubed(3))            # Works
+print(2 * cubed(4))        # Works
+
+
+# 4. Different Parameter and Return types
+def hypotenuse(a,b):
+    return ((a ** 2) + (b ** 2)) ** 0.5
+    
+print(hypotenuse(3,4))      # 5.0 not 5
+print("---------------")
+
+def xor(b1,b2):
+    return((b1 and (not b2)) or (b2 and (not b1)))  # same as (b1 != b2)
+    
+print(xor(True, True))      # F
+print(xor(True, False))     # T
+print(xor(False, True))     # T
+print(xor(False, False))    # F
+print("---------------")
+
+def isPositive(n):
+    return (n > 0)
+    
+print(isPositive(10))
+print(isPositive(-1.234))
+
+
+# 5. Function Composition
+def f(w):
+    return 10 * w
+    
+def g(x,y):
+    return f(3 * x) + y
+
+def h(z):
+    return f(g(z, f(z+1)))
+    
+print(h(1))
+
+
+# 6. Helper functions
+def onesDigit(n):
+    return n % 10
+    
+def largerOnesDigit(x,y):
+    return max(onesDigit(x),onesDigit(y))
+    
+print(largerOnesDigit(134,672))
+print(largerOnesDigit(132,674))
