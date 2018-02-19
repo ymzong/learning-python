4b7919d116488d0115cf39f8b49b95cae5495598
Yifan Cao <9102602+sunnyifan@users.noreply.github.com>
2018-02-19 00:41:40 -0500

    conditionals: inital commitment

---
+++ wk1/conditionals.py
@@ -0,0 +1,35 @@
+# 1. If statement
+def f(x):
+    print('A', end='')
+    if (x == 0):
+        print('B', end='')
+        print('C', end='')
+    print('D')
+    
+f(0)
+f(1)
+
+# These examples define abs(n), which is a nice example here but it is 
+# also a builtin function, so you do not need to define it to use it.
+
+def abs1(n):
+    if (n < 0):
+        n = -n
+    return n
+# with same-line indenting 
+def abs2(n):
+    if (n < 0): n = -n  # Only indent this way with very short lines
+    return n
+# with multiple return statment
+def abs3(n):
+    if (n < 0):
+        return -n
+    return n
+# Aside: you can do this with boolean arithmetic, but don't
+def abs4(n):
+    return (n < 0)*(-n) + (n >= 0)*(n)  # this is bad
+    
+print("abs1(5) =", abs1(5), "and abs1(-5) =", abs1(-5))
+print("abs2(5) =", abs2(5), "and abs2(-5) =", abs2(-5))
+print("abs3(5) =", abs3(5), "and abs3(-5) =", abs3(-5))
+print("abs4(5) =", abs4(5), "and abs4(-5) =", abs4(-5))
